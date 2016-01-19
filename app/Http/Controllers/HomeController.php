f5c774a01c912dcdb1e36b42a4cfb93d92477af9
Luis Vivas <luisvivas2504@gmail.com>
2016-01-18 22:51:21 -0430

    jejeps3

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,7 +26,9 @@ class HomeController extends Controller
      */
     public function create()
     {
-        //
+        $User = new Users;
+        $User->fill(Input::all());
+        $User->save(); 
     }
 
     /**
